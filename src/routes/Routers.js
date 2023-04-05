7f532d93f05e8e7db0e9ca5cf9d19744773f4363
Your Name <minhnhat14712@gmail.com>
2023-04-05 17:12:09 +0700

    first commit

---
+++ src/routes/Routers.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+import Home from '../pages/Home'
+import AllFood from '../pages/AllFood'
+import FoodDetails from '../pages/FoodDetails'
+import Cart from '../pages/Cart'
+import Checkout from '../pages/Checkout'
+import Contact from '../pages/Contact'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+
+const Routers = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<Navigate to="/home" />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/foods' element={<AllFood />} />
+        <Route path='/foods/:id' element={<FoodDetails />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/Contact' element={<Contact />} />
+
+    </Routes>
+  )
+}
+
+export default Routers
